0acf7897b638a9e12f269a8ac38f211b1d123cf0
Vlad <jeywer2747@gmail.com>
2023-08-13 01:47:06 +0300

    Added empty project

---
+++ src/main/java/ru/karelin/SensorMeasurementApiApp/SensorMeasurementApiAppApplication.java
@@ -0,0 +1,13 @@
+package ru.karelin.SensorMeasurementApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMeasurementApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMeasurementApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/karelin/SensorMeasurementApiApp/SensorMeasurementApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.karelin.SensorMeasurementApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMeasurementApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
